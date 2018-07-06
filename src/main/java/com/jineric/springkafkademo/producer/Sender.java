0ddd718d95d936b906e71c90b39db30090c08681
Eric Baek <eric.baek@daugherty.com>
2018-07-06 11:11:32 -0400

    Initial commit

---
+++ src/main/java/com/jineric/springkafkademo/producer/Sender.java
@@ -0,0 +1,18 @@
+package com.jineric.springkafkademo.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class Sender {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Sender.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String payload){
+        LOGGER.info("sending payload='{}' to topic='{}'", payload, topic);
+        kafkaTemplate.send(topic, payload);
+    }
+}
